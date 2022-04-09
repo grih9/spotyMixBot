2640177b1906f39deb89b028f90cc6e6675b33f3
Tolstikov Grigoriy <tolstikov.gn@edu.spbstu.ru>
2022-04-09 23:00:22 +0300

    no message

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import uuid
+
 import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyOAuth
@@ -10,39 +12,55 @@ scope = "user-library-read streaming app-remote-control user-library-modify play
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET,
                                                redirect_uri=SPOTIPY_REDIRECT_URI, scope=scope))
-# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET,
-#                                                redirect_uri=SPOTIPY_REDIRECT_URI))
 
-results = sp.current_user_saved_tracks()
+results = sp.recommendation_genre_seeds()
+results = sp.recommendation_genre_seeds()
 
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
-
-
-
-result = sp.current_user_top_artists(limit=100, time_range="short_term")
-for idx, item in enumerate(result['items']):
-    name = item["name"]
-    print(idx, name)
-result = sp.current_user_top_artists(limit=100, time_range="medium_term")
-for idx, item in enumerate(result['items']):
-    name = item["name"]
-    print(idx, name)
-result = sp.current_user_top_artists(limit=100, time_range="long_term")
-for idx, item in enumerate(result['items']):
-    name = item["name"]
-    print(idx, name)
-
-result = sp.current_user_top_tracks(limit=100, time_range="short_term")
-for idx, item in enumerate(result['items']):
+results = sp.recommendations(seed_genres=["classical"])
+for idx, item in enumerate(results['tracks']):
     track = item['name']
     print(idx, item['artists'][0]['name'], " – ", track)
-result = sp.current_user_top_tracks(limit=100, time_range="medium_term")
-for idx, item in enumerate(result['items']):
-    track = item['name']
-    print(idx, item['artists'][0]['name'], " – ", track)
-result = sp.current_user_top_tracks(limit=100, time_range="long_term")
-for idx, item in enumerate(result['items']):
-    track = item['name']
-    print(idx, item['artists'][0]['name'], " – ", track)
+
+# # sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET,
+# #                                                redirect_uri=SPOTIPY_REDIRECT_URI))
+# user = sp.current_user()
+# playlist_name = f"SpotyMix Bot - {uuid.uuid1()}"
+# playlist = sp.user_playlist_create(user, playlist_name)
+# tracks = sp.recommendations()
+# sp.playlist_add_items(playlist, tracks)
+#
+# results = sp.current_user_saved_tracks()
+#
+#
+#
+# for idx, item in enumerate(results['items']):
+#     track = item['track']
+#     print(idx, track['artists'][0]['name'], " – ", track['name'])
+#
+#
+#
+# result = sp.current_user_top_artists(limit=100, time_range="short_term")
+# for idx, item in enumerate(result['items']):
+#     name = item["name"]
+#     print(idx, name)
+# result = sp.current_user_top_artists(limit=100, time_range="medium_term")
+# for idx, item in enumerate(result['items']):
+#     name = item["name"]
+#     print(idx, name)
+# result = sp.current_user_top_artists(limit=100, time_range="long_term")
+# for idx, item in enumerate(result['items']):
+#     name = item["name"]
+#     print(idx, name)
+#
+# result = sp.current_user_top_tracks(limit=100, time_range="short_term")
+# for idx, item in enumerate(result['items']):
+#     track = item['name']
+#     print(idx, item['artists'][0]['name'], " – ", track)
+# result = sp.current_user_top_tracks(limit=100, time_range="medium_term")
+# for idx, item in enumerate(result['items']):
+#     track = item['name']
+#     print(idx, item['artists'][0]['name'], " – ", track)
+# result = sp.current_user_top_tracks(limit=100, time_range="long_term")
+# for idx, item in enumerate(result['items']):
+#     track = item['name']
+#     print(idx, item['artists'][0]['name'], " – ", track)
